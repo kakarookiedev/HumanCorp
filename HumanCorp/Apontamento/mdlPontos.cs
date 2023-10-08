77897f6b743776abde497da6fad03e6197bdc3ec
kakarookiedev <kauanrodrigues174@gmail.com>
2023-10-08 16:27:01 -0300

    base Pontos

---
+++ HumanCorp/Apontamento/mdlPontos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanCorp.Apontamento
+{
+    public class mdlPontos
+    {
+        public int idFuncionario { get; set; }
+        public DateTime periodo { get; set; }
+        public string hEntrada { get; set; }
+        public string hIntervalo { get; set; }
+        public string hRetorno { get; set; }
+        public string hSaida { get; set; }
+        public string descricao { get; set; }
+    }
+}
---
+++ HumanCorp/Modelo/mdlPontos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanCorp.Modelo
+{
+    class mdlPontos
+    {
+        public int idFuncionario { get; set; }
+        public DateTime periodo { get; set; }
+        public string hEntrada { get; set; }
+        public string hIntervalo { get; set; }
+        public string hRetorno { get; set; }
+        public string  hSaida { get; set; }
+        public string observacao { get; set; }
+    }
+    
+}
